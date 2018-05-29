c74a4986d5e76e25ae8965564c86a1dfd1b23877
SchultzVVhome <xultezz@gmail.com>
2018-05-29 19:11:14 -0300

    Tentativa de colocar a distribuição num histograma.

--- QRNGaussian.py
+++ QRNGaussian.py
@@ -1,5 +1,6 @@
 def QRNGGaussian(d):
     import math
+    from QRNG import GetAnysizeArray;import numpy as np
     D=GetAnysizeArray(d)
     aux=np.zeros(d)
     for i in range(0,d):    # Looping para limitar o vetor entre [0,1)
@@ -11,10 +12,28 @@ def QRNGGaussian(d):
         x[i+1]=np.sqrt(-2*math.log(D[i+1]))*math.sin(2*math.pi*D[i])
     return x
 
+
+import matplotlib.pyplot    as plt
+def plotHistogram(y,x):
+    #populationAges = [22, 55, 62, 45, 21, 22, 34, 42, 42, 4, 99, 102, 55,
+    #                  44, 66, 77, 33, 22, 99, 88, 77, 66, 55, 44, 33, 11, 23, 45, 67, 89]
+    x=QRNGGaussian(16)
+    #ids = [x for x in range(len(populationAges))]
+    # plt.bar(ids,populationAges)
+    bins = [-3.5, -3,-2.5, -2, -1.5, -1, -0.5, 0, 0.5,1.0,1.5,2,2.5,3,3.5,4]
+    plt.hist(y, x, histtype='bar', rwidth=0.8)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    plt.show()
+
+
 def Test(d):
     from QRNG import Contador
     x=QRNGGaussian(d)
     y= Contador(0,d)
     from plots import plotScatter
-    plotScatter(y,x)
-Test(10)
+    #plotScatter(y,x)
+    bins = [-3.5, -3,-2.5, -2, -1.5, -1, -0.5, 0, 0.5,1.0,1.5,2,2.5,3]
+    plotHistogram(y,bins)
+Test(100)
